26ee3cced4dceba1bab29a1cd753fe806137672e
msaaddev <mrsaadirfan@gmail.com>
2020-06-10 00:44:34 +0500

    📦 NEW: header

---
+++ utils/header.js
@@ -0,0 +1,7 @@
+// importing packages
+
+const { bgBlue } = require('chalk');
+const io = require('console-read-write');
+const pkg = require('../package.json');
+
+module.exports = () => io.write(bgBlue(`\n\n ${pkg.name} v${pkg.version} by ${pkg.author.name} `));
