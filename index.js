f5c3068a9dd4c61528574494ff2250997b793dbf
msaaddev <mrsaadirfan@gmail.com>
2020-06-10 00:45:20 +0500

    👌 IMPROVE: executable cli

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+#!/usr/bin/env node
 
+/**
+ *
+ * Author: Saad Irfan
+ * GitHub: msaaddev
+ * Twitter: https://twitter.com/msaaddev
+ */
+
+// importing files & packages
+
+const header = require('./utils/header');
+const cli = require('./utils/cli');
+const footer = require('./utils/footer');
+const clear = require('clear');
+
+
+(module.exports = () => {
+    clear();
+    header();
+    cli();
+    footer();
+})();
