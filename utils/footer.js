011582ec26fb9b29cd04f4bc5af41b3fa5913ba8
msaaddev <mrsaadirfan@gmail.com>
2020-06-10 21:14:03 +0500

    👌 IMPROVE: footer

--- utils/footer.js
+++ utils/footer.js
@@ -3,7 +3,6 @@ const io = require('console-read-write');
 const pkg = require('../package.json');
 
 module.exports = () => {
-    io.write(dim(cyan(`\n Thank you for using ${pkg.name}!`)));
     io.write(dim(cyan('\n Star (⭐️) the repo: https://github.com/msaaddev/password-gen-cli/')));
     io.write(dim(cyan(' Connect with me: https://twitter.com/MSaaddev/\n')));
 };
