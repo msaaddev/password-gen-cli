2937b32e93b881f55ca188b43952942c77fd94ee
msaaddev <mrsaadirfan@gmail.com>
2020-06-10 20:55:17 +0500

    👌 IMPROVE: password gen complete

--- utils/cli.js
+++ utils/cli.js
@@ -1,8 +1,14 @@
 // importing packages
 
 const fs = require('fs');
-
+const chalk = require('chalk');
+const io = require('console-read-write');
 
 module.exports = () => {
-	
-}
+    let pass = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*abcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (let i = 0; i < 15; i++)
+        pass += characters.charAt(Math.floor(Math.random() * charactersLength));
+    io.write(chalk.italic(`\nPassword successfully generated & copied to the clipboard: ${pass}`));
+};
